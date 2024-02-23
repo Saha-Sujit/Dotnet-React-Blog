0d0e10e50ab3fba0d1a188f0ff05474f983b53f6
Sujit Saha <sahasujit14@gmail.com>
2024-02-23 23:09:05 +0530

    Removed migration folder

--- BlogApi/DTO/LoginUserDTO.cs
+++ BlogApi/DTO/LoginUserDTO.cs
@@ -5,5 +5,6 @@ namespace UserLoginDTO.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
+        public string? Token { get; set; }
     }
 }
