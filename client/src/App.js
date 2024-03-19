0ae5501b4e44ccd5ddd172448f567209415bfe2a
Sujit Saha <sahasujit14@gmail.com>
2024-03-19 23:50:38 +0530

    added card and carousel component for testing

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "./components/ui/accordion";
+import { Card, CardContent } from "./components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./components/ui/carousel";
 
 const App = () => {
   return (
@@ -32,6 +40,23 @@ const App = () => {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
